be853e2950cfd86c74ee186f62311afc6ae556c3
JHompesc-dev <jhompesc@avans.nl>
2020-02-04 11:02:05 +0100

    https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
    physics

--- TheMaoci-Internal/UnhandledExceptionsShower/Raycast.cs
+++ TheMaoci-Internal/UnhandledExceptionsShower/Raycast.cs
@@ -35,7 +35,7 @@ namespace UnhandledException
         public static Vector3 BarrelRaycast() {
             if (Cons.Main._localPlayer.Fireport == null)
                 return Vector3.zero;
-            Physics.Linecast(Cons.Main._localPlayer.Fireport.position, Cons.Main._localPlayer.Fireport.position - Cons.Main._localPlayer.Fireport.up * Math.Infinity, out raycastHit, mask);
+            Physics.Linecast(Cons.Main._localPlayer.Fireport.position, Cons.Main._localPlayer.Fireport.position - Cons.Main._localPlayer.Fireport.up * Mathf.Infinity, out raycastHit, mask);
             return raycastHit.point;
         }
         #region IsVisible - Properly done with mask
